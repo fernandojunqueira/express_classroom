55703c89204360241a4cf642d1bf62dc13f41179
Fernandojunqueira <fernandojunqueira12@gmail.com>
2023-03-20 19:53:25 -0300

    feat: add post

--- src/index.js
+++ src/index.js
@@ -1,13 +1,70 @@
+// import express from "express";
+// import { v4 as uuidv4 } from "uuid";
+
+// const users = [];
+
+// const app = express();
+// app.use(express.json());
+
+// app.post("/user", (request, response) => {
+//   const { email, name } = request.body
+
+//   const userAlrealdyExists = users.find((user) => user.email === email);
+
+//   if (userAlrealdyExists) {
+//     return response.status(400).json({
+//       error: "This email address is already being used"
+//     });
+//   }
+
+//   const newUser = {
+//     id: v4(),
+//     email,
+//     name,
+//   }
+
+//   users.push(newUser);
+
+//   return response.status(201).json(newUser);
+// });
+
+// app.get("/", (request, response) => {
+//   return response.send("Hello Kenzie");
+// });
+
+// app.listen(3000);
 import express from "express"
-
-
-// const express = require("express")
+import { v4 } from "uuid"
+const users = []
 
 const app = express()
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    return res.send("Hello, Express!")
+app.post("/users", (request, response) => {
+    const { email, name } = request.body
+
+    const userAlrealdyExists = users.find((user) => user.email === email)
+
+    if (userAlrealdyExists) {
+        return response.status(400).json({
+          error: "This email address is already being used"
+        });
+      }
+
+    const newUser = {
+        id: v4(),
+        email,
+        name
+    }
+
+    users.push(newUser)
+
+    return response.status(201).json(newUser)
 })
+
+app.get("/", (request, response) => {
+    return response.send("Hello Kenzie");
+  });
 
 const PORT = 3000
 
