edb067ec18d1e9952c67f44b45712b9e1d1131f6
Fernandojunqueira <fernandojunqueira12@gmail.com>
2023-03-21 13:00:51 -0300

    feat: add database with postgres

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
+import express from "express"
 import { createUserController, retrieveUserController, userLoginController } from "./controllers/controllers"
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (request, response) => {
+    return response.send("Hello Kenzie");
+  });
 
 app.post("/users", createUserController)
 app.post("/login", userLoginController)
 
 app.get("/users/:id", retrieveUserController)
+
+export default app
